f43f680ccb52abc5d188f4d92b0a308db0aacef5
Genvekt <e.kivotova@gmail.com>
2018-01-31 23:14:48 +0300

    added url to profile

--- register/urls.py
+++ register/urls.py
@@ -4,8 +4,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<username>[a-zA-Z0-9]+)$', views.get_user_profile, name='user_profile'),
     url(r'^document/$', views.DocumentListView.as_view(), name='document'),
     url(r'^document/(?P<pk>\d+)$', views.DocumentDetailView.as_view(), name='document-detail'),
     url(r'^authors/$', views.AuthorsListView.as_view(), name='authors'),
     url(r'^authors/(?P<pk>\d+)$', views.AuthorDetailView.as_view(), name='author-detail'),
+    url(r'^documentinstance/$', views.DocumentInstanceView.as_view(), name='documentinstance'),
 ]
--- register/views.py
+++ register/views.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
+import Scripts
 
 # Create your views here.
 from django.shortcuts import render
@@ -37,6 +39,10 @@ class DocumentDetailView(generic.DetailView):
     model = Document
 
 
+class DocumentInstanceView(generic.ListView):
+    model = DocumentInstance
+
+
 class AuthorsListView(generic.ListView):
     model = Author
     paginate_by = 10
@@ -44,3 +50,11 @@ class AuthorsListView(generic.ListView):
 
 class AuthorDetailView(generic.DetailView):
     model = Author
+
+
+from django.shortcuts import render
+
+
+def get_user_profile(request, username):
+    user = User.objects.get(username=username)
+    return render(request, 'register/user_profile.html', {"username": user})
