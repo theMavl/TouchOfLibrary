b268bc090db653d4eb6a221375d08b76ddaac4f4
Mavl <bmcmavl@gmail.com>
2018-03-02 16:53:48 +0300

    Deleting a patron will also release his reserved books

--- library/views/patron/delete_patron.py
+++ library/views/patron/delete_patron.py
@@ -4,7 +4,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 
 from library.forms import DeletePatron
-from library.models import PatronInfo
+from library.models import PatronInfo, Reservation
 
 
 @permission_required('auth.delete_user')
@@ -15,7 +15,12 @@ def delete_patron(request, pk):
         if form.is_valid():
             current_user = User.objects.get(id=pk)
             current_patron = PatronInfo.objects.get(user_id=pk)
-
+            reservations = Reservation.objects.filter(user=current_user).all()
+            for r in reservations:
+                r.document_copy.status = 'a'
+                r.document.quantity_synced = False
+                r.document_copy.save()
+                r.document.save()
             current_user.delete()
             current_patron.delete()
 
@@ -23,4 +28,3 @@ def delete_patron(request, pk):
     else:
         form = DeletePatron(initial={'comment': ''})
         return render(request, 'library/patron_delete.html', {'form': form})
-    return redirect('document-detail', id=copy)
